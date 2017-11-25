f3580d7c7a27692e1afbd127fa12b4fc33b92bfe
Andrew Herbert <dirk214@vt.edu>
2017-11-24 20:06:48 -0500

    Add files via upload

---
+++ Crytpography HW10 Problem 1/src/SupportingFiles/Crytpography HW10 Problem 1/src/SupportingFiles/SBox.java
@@ -0,0 +1,14 @@
+package SupportingFiles;
+
+/**
+ * The SBox to be used in the encryption and decryption processes.
+ * @author Andrew Herbert
+ * @version Nov 22, 2017
+ */
+public class SBox {
+    
+    public final String[] plainTextTable = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+    
+    public String[] cipherTextTable = {"E","4","D","1","2","F","B","8","3","A","6","C","5","9","0","7"};
+    
+}
